6d1d95594ac95f23dbecebb3ebdddd625b242567
ben <ben.thorpy@gmail.com>
2024-02-02 21:59:55 +0000

    Refactor background.js: Add new message types and functions
    update getStockCount to use makeErplyRequest
    add getWarehouses message type
    save session expiry time to local storage when getting session key

--- scripts/background.js
+++ scripts/background.js
@@ -1,17 +1,28 @@
 console.log("background script");
 
 const url = 'https://1105.erply.com/api/'
-const dataFixed = "clientCode=1105&sendContentType=1&request=getProducts&getStockInfo=1"
 const warehouseID = 5000000091 // Upper Mt Gravatt
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(message.plus);
-    if (message.plus) {
+    if (message.type === 'getStockCount')  {
         getStockCount(message.plus).then(sendResponse);
+        return true;
     }
-    return true;
+    if (message.type === 'getSessionKey') {
+        getSessionKey(message.username, message.password).then(sendResponse);
+        return true; 
+    }
+    if (message.type === 'getSessionKeyInfo') {
+        getSessionKeyInfo().then(sendResponse);
+        return true;
+    }
+    if (message.type === 'getWareHouses') {
+        getWareHouses().then(sendResponse);
+        return true;
+    }
 });
-    
+
 const readLocalStorage = async (key) => {
     return new Promise((resolve, reject) => {
         chrome.storage.local.get([key], function (result) {
@@ -48,37 +59,16 @@ async function makeErplyRequest(request, parameters) {
 }
 
 async function getStockCount(plu) {
-    let sessionKey;
-
     try {
-        sessionKey = await readLocalStorage('key');
-    } catch (error) {
-        console.log(error);
-        console.log("No session key found, getting new session key");
-        const success = await getSessionKey();
-        if (success) {
-            sessionKey = await readLocalStorage('key'); // update sessionKey with the new session key
-        } else {
-            console.log("failed to get session key")
-            return "Log in to ERPLY first"
+        let sessionKey = await readLocalStorage('key');
+        let obj = await makeErplyRequest("getProducts", { getStockInfo: "1", code: plu, sessionKey: sessionKey });
+        console.log(obj)
+        if (obj.status.responseStatus == "ok") {
+            return getCountFromJSON(obj);
         }
-    };
-
-    const data = dataFixed + "&code=" + plu + "&sessionKey=" + sessionKey;
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: data,
-    });
-
-    const text = await response.text();
-    const obj = JSON.parse(text);
-
-    print(text)
-    print(obj)
+    } catch (error) {
+        console.error("Error in getting stock count:", error);
+    }
 
     if (obj.status.responseStatus == "ok") {
         return getCountFromJSON(obj);
@@ -90,36 +80,33 @@ async function getStockCount(plu) {
 
 async function getSessionKey(username, password) {
     try {
-        let obj = await makeErplyRequest("verifyUser", { username: username, password: password });
-    
-        console.log(obj);
-    
+        let obj = await makeErplyRequest("verifyUser", { username: username, password: password,  sessionLength: "43200"});
         if (obj.status.responseStatus == "ok") {
             let value = obj.records[0].sessionKey;
             chrome.storage.local.set({ key: value }).then(() => {
-                console.log("Value is set to " + value);
+                console.log("Sesion key is set to " + value);
+            });
+            let expiry = obj.status.requestUnixTime + obj.records[0].sessionLength;
+            chrome.storage.local.set({ sessionExpiry: expiry }).then(() => {
+                console.log("Sesion expires at: " + expiry);
             });
             return 1;
         } else {
-            console.log("getSessionKey: oops, something wrong");
+            console.log("getSessionKey: Oops, Invalid credentials");
             return 0;
         }
     } catch (error) {
-        console.error("Error in makeErplyRequest:", error);
+        console.error("Error in makeErplyRequest: ", error);
     }
 }
-
-
 
 // retrieves the session key information from erply server, then returns if it is expired
 async function getSessionKeyInfo() {
     try {
         let sessionKey = await readLocalStorage('key');
-        let obj = await makeErplyRequest("getSessionKeyInfo", { sessionKey: sessionKey, sessionLength: "43200"});
+        let obj = await makeErplyRequest("getSessionKeyInfo", { sessionKey: sessionKey});
         if (obj.status.responseStatus == "ok") {
-            console.log(obj);
-            console.log(Math.floor(Date.now() / 1000));
-            if (obj.records[0].expireUnixTime > Math.floor(Date.now() / 1000)){
+            if (obj.records[0].expireUnixTime > obj.status.requestUnixTime) {
                 console.log("Session key is still valid");
                 return 1;
             } else {
@@ -136,6 +123,19 @@ async function getSessionKeyInfo() {
     };
 }
 
+async function getWareHouses() {
+    try {
+        let sessionKey = await readLocalStorage('key');
+        let obj = await makeErplyRequest("getAllowedWarehouses", { sessionKey: sessionKey });
+        return obj;
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+getWareHouses();
+
 function getCountFromJSON(obj) {
     if (obj.records.length == 0) {
         return -1000;
@@ -143,5 +143,3 @@ function getCountFromJSON(obj) {
         return obj.records[0].warehouses[warehouseID].free;
     }
 }
-
-
