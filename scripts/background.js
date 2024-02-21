f4c8ec54c930dba6615bc3596965c461578d8a8b
ben <ben.thorpy@gmail.com>
2024-02-21 18:43:18 +1000

    Refactor stock count functionality and add warehouse selection popup
    Format backgorund.js
    remove excess console.log() statements
    make maxSessionLength a const
    utilize storageManager in background.js
    update getCountFromJSON to return a ditionary of warehouses to counts
    add getWarehousesfromJSON to parse all available warehouses from json obj

--- scripts/background.js
+++ scripts/background.js
@@ -1,154 +1,159 @@
 console.log("background script");
 
-const url = 'https://1105.erply.com/api/'
-const warehouseID = 5000000091 // Upper Mt Gravatt
+importScripts("shared.js");
 
-chrome.storage.local.clear(function() {
-    var error = chrome.runtime.lastError;
-    if (error) {
-        console.error(error);
-    } else {
-        console.log('Local storage cleared');
-    }
-});
+const url = "https://1105.erply.com/api/";
+const maxSessionLength = "43200";
+
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(message.plus);
-    if (message.type === 'getStockCount')  {
-        getStockCount(message.plus).then(sendResponse);
-        return true;
-    }
-    if (message.type === 'getSessionKey') {
-        getSessionKey(message.username, message.password).then(sendResponse);
-        return true; 
-    }
-    if (message.type === 'getSessionKeyInfo') {
-        getSessionKeyInfo().then(sendResponse);
-        return true;
-    }
-    if (message.type === 'getWareHouses') {
-        getWareHouses().then(sendResponse);
-        return true;
-    }
+  if (message.type === "getStockCount") {
+    getStockCount(message.plus).then(sendResponse);
+    return true;
+  }
+  if (message.type === "getSessionKey") {
+    getSessionKey(message.username, message.password).then(sendResponse);
+    return true;
+  }
+  if (message.type === "getSessionKeyInfo") {
+    getSessionKeyInfo().then(sendResponse);
+    return true;
+  }
+  if (message.type === "getWareHouses") {
+    getWareHouses().then(sendResponse);
+    return true;
+  }
 });
 
-const readLocalStorage = async (key) => {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.get([key], function (result) {
-            if (result[key] === undefined) {
-                reject(`No value found for key: ${key}`);
-            } else {
-                resolve(result[key]);
-            }
-        });
-    });
-};
-
 async function makeErplyRequest(request, parameters) {
-    let data = "clientCode=1105&sendContentType=1" + "&request=" + request
-    
-    for (let key in parameters) {
-        data += "&" + key + "=" + parameters[key];
-    }
+  let data = "clientCode=1105&sendContentType=1" + "&request=" + request;
 
-    console.log(data)
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: data,
-    });
+  for (let key in parameters) {
+    data += "&" + key + "=" + parameters[key];
+  }
 
-    const text = await response.text();
-    const obj = JSON.parse(text);
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: data,
+  });
 
-    return obj;
+  const text = await response.text();
+  const obj = JSON.parse(text);
+
+  return obj;
 }
 
 async function getStockCount(plu) {
-    try {
-        let sessionKey = await readLocalStorage('key');
-        let obj = await makeErplyRequest("getProducts", { getStockInfo: "1", code: plu, sessionKey: sessionKey });
-        console.log(obj)
-        if (obj.status.responseStatus == "ok") {
-            return getCountFromJSON(obj);
-        }
-    } catch (error) {
-        console.error("Error in getting stock count:", error);
-    }
-
+  try {
+    let sessionKey = await StorageManager.readLocalStorage("key");
+    let obj = await makeErplyRequest("getProducts", {
+      getStockInfo: "1",
+      code: plu,
+      sessionKey: sessionKey,
+    });
+    console.log(obj);
     if (obj.status.responseStatus == "ok") {
-        return getCountFromJSON(obj);
+      return getCountsFromJSON(obj);
     } else {
-        console.log("getStockCount: something wrong");
-        return "something went wrong";
+      console.log("Error in getting stock count");
+      return null;
     }
+  } catch (error) {
+    console.error("Error in getting stock count:", error);
+    return null;
+  }
 }
 
 async function getSessionKey(username, password) {
-    try {
-        let obj = await makeErplyRequest("verifyUser", { username: username, password: password,  sessionLength: "43200"});
-        if (obj.status.responseStatus == "ok") {
-            let value = obj.records[0].sessionKey;
-            chrome.storage.local.set({ key: value }).then(() => {
-                console.log("Sesion key is set to " + value);
-            });
-            let expiry = obj.status.requestUnixTime + obj.records[0].sessionLength;
-            chrome.storage.local.set({ sessionExpiry: expiry }).then(() => {
-                console.log("Sesion expires at: " + expiry);
-            });
-            return true;
-        } else {
-            console.log("getSessionKey: Oops, Invalid credentials");
-            return false;
-        }
-    } catch (error) {
-        console.error("Error in makeErplyRequest: ", error);
+  try {
+    let obj = await makeErplyRequest("verifyUser", {
+      username: username,
+      password: password,
+      sessionLength: maxSessionLength,
+    });
+    if (obj.status.responseStatus == "ok") {
+      let value = obj.records[0].sessionKey;
+      setLocalStorage("key", value);
+      let expiry = obj.status.requestUnixTime + obj.records[0].sessionLength;
+      setLocalStorage("sessionExpiry", expiry);
+      return true;
+    } else {
+      console.log("getSessionKey: Oops, Invalid credentials");
+      return false;
     }
+  } catch (error) {
+    console.error("Error in makeErplyRequest: ", error);
+  }
 }
 
 // retrieves the session key information from erply server, then returns if it is expired
 async function getSessionKeyInfo() {
-    try {
-        let sessionKey = await readLocalStorage('key');
-        let obj = await makeErplyRequest("getSessionKeyInfo", { sessionKey: sessionKey});
-        if (obj.status.responseStatus == "ok") {
-            if (obj.records[0].expireUnixTime > obj.status.requestUnixTime) {
-                console.log("Session key is still valid");
-                return true;
-            } else {
-                console.log("Session key is expired");
-                return false;
-            }
-        } else {
-            console.log("Incorrect session key");
-            return false;
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    let sessionKey = await StorageManager.readLocalStorage("key");
+    let obj = await makeErplyRequest("getSessionKeyInfo", {
+      sessionKey: sessionKey,
+    });
+    if (obj.status.responseStatus == "ok") {
+      if (obj.records[0].expireUnixTime > obj.status.requestUnixTime) {
+        console.log("Session key is still valid");
+        return true;
+      } else {
+        console.log("Session key is expired");
         return false;
-    };
+      }
+    } else {
+      console.log("Incorrect session key");
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
 async function getWareHouses() {
-    try {
-        let sessionKey = await readLocalStorage('key');
-        let obj = await makeErplyRequest("getAllowedWarehouses", { sessionKey: sessionKey });
-        return obj;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    let sessionKey = await StorageManager.readLocalStorage("key");
+    let obj = await makeErplyRequest("getAllowedWarehouses", {
+      sessionKey: sessionKey,
+    });
+    console.log(obj);
+    return await getWarehousesfromJSON(obj);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+function getWarehousesfromJSON(obj) {
+  let warehouses = obj.records;
+  let warehouseArray = [];
+  // cycle through the warehouses and get the name and id
+  console.log(warehouses);
+  for (let key in warehouses) {
+    warehouseArray.push({
+      id: warehouses[key].warehouseID,
+      name: warehouses[key].name,
+    });
+  }
+
+  return warehouseArray;
 }
 
 getWareHouses();
 
-function getCountFromJSON(obj) {
-    if (obj.records.length == 0) {
-        return -1000;
-    } else {
-        return obj.records[0].warehouses[warehouseID].free;
-    }
+async function getCountsFromJSON(obj) {
+  console.log(obj);
+  console.log(obj.records[0].warehouses);
+  let warehouse_counts = {};
+
+  for (let key in obj.records[0].warehouses) {
+    warehouse_counts[key] = obj.records[0].warehouses[key].free;
+  }
+  console.log(warehouse_counts);
+  return warehouse_counts;
 }
