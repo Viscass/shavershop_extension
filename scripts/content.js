38eff677e49201f4d5a01aa0fa318946f2950b22
ben <ben.thorpy@gmail.com>
2024-02-22 15:16:05 +1000

    Refactor code to improve performance and readability
    create classes for dom elements

--- scripts/content.js
+++ scripts/content.js
@@ -1,46 +1,7 @@
-
-let stockCount; // This will be the stock count element
-let loginForm; // This will be the login form
-let container; // This will be the container block
-
-const readLocalStorage = async (key) => {
-  return new Promise((resolve, reject) => {
-    chrome.storage.local.get([key], function (result) {
-      if (result[key] === undefined) {
-        reject(`No value found for key: ${key}`);
-      } else {
-        resolve(result[key]);
-      }
-    });
-  });
-};
-
-async function updateStockCount(plu) {
-  if (!(await checkSession())) {
-    console.log("Invalid session key");
-    return;
-  }
-
-  chrome.runtime.sendMessage(
-    { type: "getStockCount", plus: plu },
-    (response) => {
-      var count = response;
-      if (stockCount) {
-        stockCount.textContent = "Stock count: " + count;
-      }
-    }
-  );
-}
-
-async function getWareHouses() {
-  const warehouses = await chrome.runtime.sendMessage({ type: "getWareHouses" });
-  return warehouses;
-}
-
 async function checkSession() {
   try {
-    let sessionKey = await readLocalStorage("key");
-    let sessionExpiry = await readLocalStorage("sessionExpiry");
+    let sessionKey = await StorageManager.readLocalStorage("key");
+    let sessionExpiry = await StorageManager.readLocalStorage("sessionExpiry");
     if (sessionKey === undefined || sessionExpiry < Date.now() / 1000) {
       return false;
     }
@@ -51,165 +12,207 @@ async function checkSession() {
   }
 }
 
-async function createStockCountContainer() {
-  // Create the container block
-  if (!container) {
-    container = document.createElement("div");
-    container.style.position = "fixed";
-    container.style.right = "0";
-    container.style.top = "230px";
-    container.style.transform = "translateY(-50%)";
-    container.style.backgroundColor = "#fff";
-    container.style.padding = "10px";
-    container.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
-    container.style.zIndex = "1000";
-    container.style.width = "330px";
+class Content {
+  constructor(plu) {
+    this.stockCount = null;
+    this.loginForm = null;
+    this.container = null;
+    this.stockInfo = null;
+    this.plu = plu;
+
+    this.createMainContainer();
+  }
+
+  async createMainContainer() {
+    // Create the container block
+    this.container = document.createElement("div");
+    this.container.style.position = "fixed";
+    this.container.style.right = "0";
+    this.container.style.top = "230px";
+    this.container.style.transform = "translateY(-50%)";
+    this.container.style.backgroundColor = "#fff";
+    this.container.style.padding = "10px";
+    this.container.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    this.container.style.zIndex = "1000";
+    this.container.style.width = "330px";
     // Add the container block to the body
-    document.body.appendChild(container);
+    document.body.appendChild(this.container);
+
+    this.stockCount = new StockCount(this.container);
+    this.loginForm = new LoginForm(this.container, this.logInSuccess.bind(this));
+
+    const sessionValid = await checkSession();
+
+    if (sessionValid) {
+      this.logInSuccess();
+    } else {
+      this.loginForm.show();
+    }
   }
+
+  async logInSuccess() {
+    await this.fetchStockInfo(this.plu);
+    this.stockCount.updateStockCount(this.stockInfo);
+    this.stockCount.show();
+    this.loginForm.hide();
+  }
+
+  async fetchStockInfo(plu) {
+    if (!(await checkSession())) {
+      console.log("Invalid session key");
+      return;
+    }
   
+    chrome.runtime.sendMessage(
+      { type: "getWarehousesStockCount", plus: plu },
+      (response) => {
+        this.stockInfo = response;
+        console.log("Stock info: ", response);          
 
-  if (!stockCount) {
-    stockCount = createStockCountElement(container);
+      }
+    );
   }
 
-  if (!loginForm) {
-    loginForm = createLoginForm(container);
+}
+
+class LoginForm {
+  constructor(container, logInSuccess) {
+    this.container = container;
+    this.logInSuccess = logInSuccess;
+    this.form = this.createLoginForm();
   }
 
-  createWarehouseSelectButton(container);
+  createLoginForm() {
+    // Create the login form
+    const loginForm = document.createElement("form");
+    loginForm.style.padding = "20px";
+    this.container.appendChild(loginForm);
 
-  updateElements();
-}
+    // Create the username field
+    this.usernameField = document.createElement("input");
+    this.usernameField.type = "text";
+    this.usernameField.name = "username";
+    this.usernameField.placeholder = "Username";
+    loginForm.appendChild(this.usernameField);
 
-function createStockCountElement(container) {
-  // Create the stock count element
-  const stockCount = document.createElement("p");
-  stockCount.textContent = "Stock count: ?"; // update this value later
-  stockCount.style.display = 'none'; // hide initially
-  container.appendChild(stockCount);
-  return stockCount;
-}
+    // Create the password field
+    this.passwordField = document.createElement("input");
+    this.passwordField.type = "password";
+    this.passwordField.name = "password";
+    this.passwordField.placeholder = "Password";
+    loginForm.appendChild(this.passwordField);
 
-function createLoginForm(container) {
-  // Create the login form
-  const loginForm = document.createElement("form");
-  loginForm.style.padding = "20px";
-  container.appendChild(loginForm);
+    // Create the submit button
+    const submitButton = document.createElement("input");
+    submitButton.type = "submit";
+    submitButton.value = "Log in";
+    loginForm.appendChild(submitButton);
 
-  const logInText = document.createElement("p");
-  logInText.textContent = "Please log in to see stock count.";
-  logInText.style.marginBottom = "10px";
-  loginForm.appendChild(logInText);
+    // Handle form submission
+    loginForm.addEventListener("submit", this.handleSubmit.bind(this));
 
-  // Create the username field
-  const usernameField = document.createElement("input");
-  usernameField.type = "text";
-  usernameField.name = "username";
-  usernameField.placeholder = "Username";
-  loginForm.appendChild(usernameField);
+    return loginForm;
+  }
 
-  // Create the password field
-  const passwordField = document.createElement("input");
-  passwordField.type = "password";
-  passwordField.name = "password";
-  passwordField.placeholder = "Password";
-  loginForm.appendChild(passwordField);
-
-  // Create the submit button
-  const submitButton = document.createElement("input");
-  submitButton.type = "submit";
-  submitButton.value = "Log in";
-  loginForm.appendChild(submitButton);
-
-  // Hide login form
-  loginForm.style.display = "none";
-
-  // Handle form submission
-  loginForm.addEventListener("submit", (event) => {
+  handleSubmit(event) {
     event.preventDefault();
     // Handle login here
     chrome.runtime.sendMessage(
       {
-        type: "getSessionKey",
-        username: usernameField.value,
-        password: passwordField.value,
+        type: "logIn",
+        username: this.usernameField.value,
+        password: this.passwordField.value,
       },
       (response) => {
-        updateElements();
-        if (response) {
+        if (response.success) {
           console.log("Login successful");
-        } else {        
+          this.logInSuccess();
+        } else {
           console.log("Login failed");
-          alert("Login failed")
+          alert("Login failed");
         }
       }
     );
-  });
-
-  return loginForm;
-}
-
-function createWarehouseSelectButton(container) {
-  const button = document.createElement("button");
-  button.textContent = "Select Warehouse";
-  button.addEventListener("click", showPopup);
-  container.appendChild(button);
-}
-
-// Add this function to show the popup
-async function showPopup() {
-  const popup = document.createElement("div");
-  popup.style.position = "fixed";
-  popup.style.left = "50%";
-  popup.style.top = "50%";
-  popup.style.transform = "translate(-50%, -50%)";
-  popup.style.backgroundColor = "#fff";
-  popup.style.padding = "20px";
-  popup.style.zIndex = "1001";
-
-  const warehouses = await getWareHouses();
-  const list = document.createElement("ul");
-  warehouses.forEach((warehouse) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = warehouse;
-    listItem.addEventListener("click", () => selectWarehouse(warehouse));
-    list.appendChild(listItem);
-  });
-  popup.appendChild(list);
-
-  document.body.appendChild(popup);
-}
-
-// Add this function to handle warehouse selection
-function selectWarehouse(warehouse) {
-  chrome.storage.local.set({ selectedWarehouse: warehouse }, function() {
-    console.log('Warehouse selected: ' + warehouse);
-  });
-}
-
-
-async function updateElements() {
-  // Check the session status
-  const sessionValid = await checkSession();
-
-  // Update the visibility of the loginForm and stockCount based on the session status
-  if (loginForm) {
-    loginForm.style.display = sessionValid ? 'none' : 'block';
   }
 
-  if (stockCount) {
-    stockCount.style.display = sessionValid ? 'block' : 'none' ;
+  show() {
+    this.form.style.display = "block";
   }
 
-  // If the session is valid, update the stock count
-  if (sessionValid) {
-    const plu = getCurrentPluFromUrl();
-    if (plu) {
-      updateStockCount(plu);
-    }
+  hide() {
+    this.form.style.display = "none";
+  }
+
+}
+
+class StockCount {
+  constructor(container) {
+    this.container = container;
+    this.warehouseID = 5000000091; // Upper Mt Gravatt;
+    this.createStockCountElement();
+  }
+
+  createStockCountElement() {
+    // Create the stock count element
+    this.stockCount = document.createElement("p");
+    this.stockCount.textContent = "Stock count: ?"; // update this value later
+    this.stockCount.style.display = "none"; // hide initially
+    this.container.appendChild(this.stockCount);
+  }
+
+  updateStockCount(stockInfo) {
+    this.stockCount.textContent = "Stock count: " + stockInfo[this.warehouseID];
+  }
+  
+  show() {
+    this.stockCount.style.display = "block";
+  }
+
+  hide() {
+    this.stockCount.style.display = "none";
   }
 }
+
+// async function showPopup() {
+//   const popup = document.createElement("div");
+//   popup.style.position = "fixed";
+//   popup.style.left = "50%";
+//   popup.style.top = "50%";
+//   popup.style.transform = "translate(-50%, -50%)";
+//   popup.style.backgroundColor = "#eee";
+//   popup.style.padding = "20px";
+//   popup.style.zIndex = "1001";
+//   popup.style.maxHeight = "400px"; // Set a maximum height
+//   popup.style.overflowY = "auto"; // Enable vertical scrolling
+
+//   const warehouseSelectText = document.createElement("p");
+//   warehouseSelectText.textContent = "Select a warehouse:";
+//   warehouseSelectText.style.marginBottom = "10px";
+//   warehouseSelectText.appendChild(popup);
+
+//   chrome.runtime.sendMessage({ type: "getWareHouses" }, (warehouses) => {
+//     console.log(warehouses);
+//     const list = document.createElement("ul");
+//     warehouses.forEach((warehouse) => {
+//       const listItem = document.createElement("li");
+//       listItem.textContent = warehouse.name;
+//       listItem.addEventListener("click", () => selectWarehouse(warehouse));
+//       list.appendChild(listItem);
+//     });
+//     popup.appendChild(list);
+//   });
+
+//   document.body.appendChild(popup);
+// }
+
+// // Add this function to handle warehouse selection
+// function selectWarehouse(warehouse) {
+//   chrome.storage.local.set({ warehouse: warehouse }, function () {
+//     console.log(
+//       "Warehouse selected: " + warehouse.name + " (" + warehouse.id + ")"
+//     );
+//   });
+// }
 
 function getCurrentPluFromUrl() {
   var plu = /[0-9]{6}/.exec(window.location.href);
@@ -222,5 +225,5 @@ function checkURLforPlu() {
 }
 
 if (checkURLforPlu()) {
-  createStockCountContainer();
+  new Content(getCurrentPluFromUrl());
 }
